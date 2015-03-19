39b0320ee3bc42ef649ea253afbd1c0c83152bd1
bryal <96.bryal@gmail.com>
2015-03-19 22:21:57 +0100

    C vtables with macro and add IDXGIFactory

--- src/main.rs
+++ src/main.rs
@@ -26,7 +26,7 @@ extern crate libc;
 extern crate winapi;
 
 use libc::c_void;
-use winapi::{ REFIID, GUID, HRESULT };
+use winapi::{ REFIID, GUID, HRESULT, ULONG, UINT };
 
 
 macro_rules! define_guid {
@@ -39,6 +39,52 @@ macro_rules! define_guid {
 // DEFINE_GUID(IID_IDXGIFactory1,0x770aae78,0xf26f,0x4dba,0xa8,0x29,0x25,0x3c,0x83,0xd1,0xb3,0x87);
 define_guid!(IID_IDXGIFactory1,0x770aae78,0xf26f,0x4dba,[0xa8,0x29,0x25,0x3c,0x83,0xd1,0xb3,0x87]);
 
+macro_rules! c_vtable {
+	($sn:ident of $sparent:ty {
+		$(fn $mn:ident($($argn:ident: $argt:ty),*) -> $rt:ty),*
+	}) => {
+		struct $sn {
+			$(
+				$mn: Option<unsafe extern "system" fn(
+					this: *mut $sparent, $($argn: $argt),*) -> $rt>
+			),*
+		}
+	}
+}
+
+#[repr(C)]
+struct IDXGIFactory {
+	vtable: *mut IDXGIFactoryVtbl
+}
+
+#[repr(C)] c_vtable!(
+IDXGIFactoryVtbl of IDXGIFactory {
+	fn QueryInterface(riid: REFIID, object: *mut *mut c_void) -> HRESULT,
+
+	fn AddRef() -> ULONG,
+
+	fn Release() -> ULONG,
+
+	fn SetPrivateData(name: REFGUID, data_size: UINT, data: *const c_void) -> HRESULT,
+
+	fn SetPrivateDataInterface(name: REFGUID, unknown: *const IUnknown) -> HRESULT,
+
+	fn GetPrivateData(name: REFGUID, data_size: UINT, data: *mut c_void) -> HRESULT,
+
+	fn GetParent(riid: REFIID, parent: *mut *mut c_void) -> HRESULT,
+
+	fn EnumAdapters(adapter_i: UINT, out_adapter: *mut *mut IDXGIAdapter) -> HRESULT,
+
+	fn MakeWindowAssociation(window_handle: HWND, flags: UINT) -> HRESULT,
+
+	fn GetWindowAssociation(out_window_handle: *mut HWND) -> HRESULT,
+
+	fn CreateSwapChain(device: *mut IUnknown, desc: *mut DXGI_SWAP_CHAIN_DESC,
+		out_swapchain: *mut *mut IDXGISwapChain) -> HRESULT,
+
+	fn CreateSoftwareAdapter(module: HMODULE, out_adapter: *mut *mut IDXGIAdapter) -> HRESULT
+});
+
 #[link(name="dxgi")]
 extern "C" {
 	#![allow(dead_code)]
@@ -46,9 +92,8 @@ extern "C" {
 	fn CreateDXGIFactory1(riid: REFIID, ppFactory: *mut *mut c_void) -> HRESULT;
 }
 
-#[test]
-fn test() {
+fn main() {
 	use std::ptr;
-	let mut p: *mut c_void = ptr::null_mut();
-	println!("p: {}", unsafe { CreateDXGIFactory1(&IID_IDXGIFactory1, &mut p) });
+	let mut factory: *mut c_void = ptr::null_mut();
+	assert_eq!(0, unsafe { CreateDXGIFactory1(&IID_IDXGIFactory1, &mut factory) });
 }
