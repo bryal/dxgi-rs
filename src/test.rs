38c2836e819b9aeddbb50db56d209eda940e02da
bryal <96.bryal@gmail.com>
2015-04-08 23:53:41 +0200

    Add trait calls in test

--- src/test.rs
+++ src/test.rs
@@ -1,21 +1,22 @@
-use super::{ IDXGIFactory1, IID_IDXGIFactory1, CreateDXGIFactory1 };
-use libc::c_void;
+use super::{ IDXGIFactory1, IUnknownT, IID_IDXGIFactory1, CreateDXGIFactory1 };
 use std::ptr;
 
 #[test]
 fn test() {
 	unsafe {
+		let factory = {
+			let mut factory: *mut _ = ptr::null_mut();
+			assert_eq!(0, CreateDXGIFactory1(&IID_IDXGIFactory1, &mut factory));
+			factory as *mut IDXGIFactory1 };
 
-	let factory = {
-		let mut factory: *mut c_void = ptr::null_mut();
-		assert_eq!(0, CreateDXGIFactory1(&IID_IDXGIFactory1, &mut factory));
-		factory as *mut IDXGIFactory1 };
+		assert!(factory as usize != 0);
 
-	assert!(factory as usize != 0);
+		println!("IsCurrent: {}", c_mtdcall!(factory,->IsCurrent()) != 0);
 
-	println!("IsCurrent: {}", c_mtdcall!(factory,->IsCurrent()) != 0);
-	assert_eq!(c_mtdcall!(factory,->AddRef()), 2);
-	assert_eq!(c_mtdcall!(factory,->Release()), 1);
+		assert_eq!(c_mtdcall!(factory,->AddRef()), 2);
+		assert_eq!((*factory).AddRef(), 3);
 
+		assert_eq!(c_mtdcall!(factory,->Release()), 2);
+		assert_eq!((*factory).Release(), 1);
 	}
 }
